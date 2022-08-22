8292a85a945363e219857efb1f38f955a08b90d3
Ferdjani Zakaria <ferdjani.abdelkaderzakaria@gmail.com>
2022-08-22 14:41:10 +0100

    add initialize function removed 'game' function and reworked 'playRound' function to show result on screen

--- index.js
+++ index.js
@@ -1,31 +1,76 @@
+const btnChoice = document.querySelectorAll('.btnChoice');
+const resultDisplay = document.querySelector('#round_result');
+const playerPoint = document.querySelector('#player_score');
+const computerpoint = document.querySelector('#computer_score');
+
+const resultContainer = document.querySelector('#game_result')
+
+let playerScore, computerScore;
+
+const startGame = () => {
+    playerScore = 0;
+    computerScore = 0;
+    resultContainer.textContent = "";
+}
+
+window.onload = startGame;
+
+
 const getComputerChoice = () =>{
     let choice = ["Rock", "Paper", "Scissors"];
     return choice[Math.floor(Math.random()*3)];
 }
 
-const playRound = (playerSelection, computerSelection) =>{
-    let good_eng_playerSelection = playerSelection.toUpperCase().slice(0,1).concat(playerSelection.slice(1).toLowerCase());
-    return  good_eng_playerSelection === computerSelection ? "Draw!" :
-            (good_eng_playerSelection === "Rock" && computerSelection === "Scissors") || 
-            (good_eng_playerSelection === "Paper" && computerSelection === "Rock") ||
-            (good_eng_playerSelection === "Scissors" && computerSelection === "Paper") ?  `You win! ${good_eng_playerSelection} beats ${computerSelection}` :
-            `You lose! ${computerSelection} beats ${good_eng_playerSelection}`
+const playRound = (playerSelection) =>{
+
+    let computerSelection = getComputerChoice()
     
+    if (playerSelection === computerSelection) {
+                resultDisplay.textContent = 'Draw!'
+                playerPoint.textContent = `Humain player score : ${playerScore}`;
+                computerpoint.textContent = `My score : ${computerScore}`;
+    }
+    // checking if the player won
+    else if ((playerSelection === "Rock" && computerSelection === "Scissors") || 
+            (playerSelection === "Paper" && computerSelection === "Rock") ||
+            (playerSelection === "Scissors" && computerSelection === "Paper"))     {
+        
+                resultDisplay.textContent =`You win the round! ${playerSelection} beats ${computerSelection}`;
+                playerScore++;
+                playerPoint.textContent = `Humain player score : ${playerScore}`;
+                computerpoint.textContent = `My score : ${computerScore}`;
+    }
+    else  {
+                resultDisplay.textContent =`You lose the round! ${computerSelection} beats ${playerSelection}`;
+                computerScore++;
+                playerPoint.textContent = `Humain player score : ${playerScore}`;
+                computerpoint.textContent = `My score : ${computerScore}`;;
+
+    }
+
+    if (playerScore === 5) {
+        resultContainer.textContent = "You win the game!";
+    }
+
+    if (computerScore === 5) {
+        resultContainer.textContent = "I (the computer) win!";
+    }
+
 }
 
-const game = () =>{
-    let playerScore = 0, computerScore = 0;
-    for (let i = 0; i<5; i++){
-        let playerSelection = prompt("select between : Rock, Paper and Scissors");
-        let round_result = playRound(playerSelection, getComputerChoice());
-        console.log(round_result)
-            if (round_result.includes("win!")){
-                playerScore++
-            }
-            if (round_result.includes("lose!")){
-                computerScore++
-            }
+
+btnChoice.forEach((btn)=>{
+    btn.addEventListener('click', function (e) {
+
+        if(playerScore === 5 || computerScore === 5){
+            startGame();
+            playRound(e.target.id) 
+        }
+
+       else{
+        playRound(e.target.id) 
+       }
+
     }
-    return  playerScore > computerScore ? "You win the game!" :
-            playerScore < computerScore ? "I (the computer) win!" : "The game was a draw!"
-}
+)
+})
